5d11dca5301cc4e405a05ac150d1e8f7f0030f7c
dilysbi <ngohung2395@gmail.com>
2021-12-19 23:40:56 +0700

    Update config.js

--- root/config.js
+++ root/config.js
@@ -5,5 +5,5 @@
   "end_round_2":       "Dec 19 2021 23:50:00 GMT+0700",
   "end_waiting_claim": "Dec 19 2021 23:55:00 GMT+0700",
   "count_round_1": "1000",
-  "count_round_2": "1500"
+  "count_round_2": "2500"
 }
