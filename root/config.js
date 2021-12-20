a1aef75ccc8aeb7df6f80414b8878d1946a75d2b
dilysbi <ngohung2395@gmail.com>
2021-12-20 20:13:33 +0700

    Update config.js

--- root/config.js
+++ root/config.js
@@ -2,7 +2,7 @@
   "isCheckWhiteList": 0,
   "start":             "Dec 20 2021 19:31:00 GMT+0700",
   "end_round_1":       "Dec 20 2021 19:32:00 GMT+0700",
-  "end_round_2":       "Dec 20 2021 19:33:00 GMT+0700",
+  "end_round_2":       "Dec 22 2021 19:33:00 GMT+0700",
   "end_waiting_claim": "Dec 20 2021 19:34:00 GMT+0700",
   "count_round_1": 1000,
   "count_round_2": 2500
