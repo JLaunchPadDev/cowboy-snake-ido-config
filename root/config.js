4477450df0d9c5cf2a7cf902e49d76bd634cd818
dilysbi <ngohung2395@gmail.com>
2021-12-21 14:03:50 +0700

    Update config.js

--- root/config.js
+++ root/config.js
@@ -1,5 +1,5 @@
 {
-  "isCheckWhiteList": 1,
+  "isCheckWhiteList": 0,
   "start":             "Dec 21 2021 07:00:00 GMT",
   "end_round_1":       "Dec 21 2021 07:00:00 GMT",
   "end_round_2":       "Dec 23 2021 21:20:00 GMT",
